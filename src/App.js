6346baa0f77610b67843e4a2f43937806bd82be2
Joao Almeida <joaoalmeida11@ua.pt>
2021-01-31 14:04:38 +0000

    useState

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Max', age: 28 },
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 },
     ],
-  };
-  switchNameHandler = () => {
+  });
+  const switchNameHandler = () => {
     // console.log('Was clicker');
-    this.setState({
+    // setPersonsState replaces what personsState had instead of merging it
+    setPersonsState({
       persons: [
         { name: 'Maximilian', age: 28 },
         { name: 'Manu', age: 29 },
@@ -20,28 +21,27 @@ class App extends Component {
       ],
     });
   };
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
-}
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
 
 export default App;
